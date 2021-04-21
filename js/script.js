e4294e54a0c3f5a191d60b0f34f7472e9932d810
ramoslucas <lucasalbuquerque5412@gmail.com>
2021-04-21 23:17:30 +0100

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,87 @@
+alert("Hello")
+/*
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+const sayHello = function () {
+
+    console.log('Hello');
+}
+
+const cars = ["Saab", "Volvo", "BMW"];
+
+cars.forEach(i => console.log(i));
+cars.forEach(element => console.log(element));
+
+console.log(cars[0]);
+
+//traditional function
+function (a){
+
+    return a + 100;
+}
+
+//arrow function
+a => a + 100;
+
+//traditional function
+function (a,b){
+
+    return a + b + 100;
+}
+
+//arrow function
+(a,b) => a + b + 100;
+
+//arrow function
+let a = 4;
+let b = 2;
+() => a + b + 100;
+
+//traditional function
+functon(a,b){
+    let chuck = 42;
+    return a + b + chuck;
+}
+
+//arrow function
+(a,b) => {
+    let chuck = 42;
+    return a + b + chuck;
+}
+
+
+
+function myfunction(arg1, arg2){
+    //function body
+}
+
+const myOtherFunction = function(arg1, arg2){
+    //function body
+}
+
+const add = function (x,y){
+
+    return sum = x + y;
+   
+}
+
+const add = (x,y) => x + y;
+
+//--------------------------------------------------------------------------
+
+const twiceSize = function (number){
+
+    return number * 2;
+}
+
+const twiceSize = number => number * 2;
+
+*/
+
